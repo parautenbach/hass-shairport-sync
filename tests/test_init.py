be3cc2a9337db78e3290c0dd810d74049580fa3b
parautenbach <parautenbach@gmail.com>
2020-09-20 17:36:13 +0200

    Trying the context syntax in order to fix the action again (and debugging the test at the same time)

--- tests/test_init.py
+++ tests/test_init.py
@@ -6,4 +6,6 @@ DOMAIN = "media_player"
 
 async def test_async_setup(hass):
     """Test the component gets setup."""
-    assert await async_setup_component(hass, DOMAIN, {}) is True
+    x = await async_setup_component(hass, DOMAIN, {})
+    print(x)
+    assert x is True
